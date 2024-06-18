7e78b9106893433cbdd4ed0169ffd37aacaa5f3a
Dan Braun <dan@apolloresearch.ai>
2024-06-18 11:24:41 +0000

    Fix sparsity loss

--- weight_factors/scripts/tms_factors.py
+++ weight_factors/scripts/tms_factors.py
@@ -11,7 +11,7 @@ from matplotlib import collections as mc
 from matplotlib import colors as mcolors
 from torch import nn
 from torch.nn import functional as F
-from tqdm import trange
+from tqdm import tqdm, trange
 
 
 # %%
@@ -28,6 +28,13 @@ class Config:
     n_instances: int
     # Number of parameter factors
     k: int
+    n_batch: int
+    steps: int
+    print_freq: int
+    lr: float
+    lr_scale: Callable[[int, int], float]
+    pnorm: float
+    sparsity_coeff: float
 
 
 class Model(nn.Module):
@@ -132,12 +139,28 @@ def optimize(
             opt.zero_grad(set_to_none=True)
             batch = model.generate_batch(n_batch)
             out, h_0, h_1 = model(batch)
+
+            # Reconstruction loss
             error = model.importance * (batch.abs() - out) ** 2
             recon_loss = einops.reduce(error, "b i f -> i", "mean").sum()
+
+            # Sparsity loss
+            out_dotted = (model.importance * (out**2)).sum()
+            # Get the gradient of out_dotted w.r.t h_0 and h_1
+            grad_h_0, grad_h_1 = torch.autograd.grad(out_dotted, (h_0, h_1), create_graph=True)
+            sparsity_inner = grad_h_0.detach() * h_0 + grad_h_1.detach() * h_1  # batch, instance, k
+
             sparsity_loss = einops.reduce(
-                (h_0 + h_1).norm(p=pnorm, dim=-1), "b i -> i", "mean"
+                sparsity_inner.norm(p=pnorm, dim=-1), "b i -> i", "mean"
             ).sum()
+
             loss = recon_loss + sparsity_coeff * sparsity_loss
+            if step % print_freq == 0:
+                tqdm.write(f"Reconstruction loss: {recon_loss.item()}")
+                tqdm.write(f"Sparsity loss: {sparsity_loss.item()}")
+                # tqdm.write(f"sparsity_inner final instance: {sparsity_inner[:5, -1, :]}")
+                # tqdm.write(f"grad_h_0 times h_0: {grad_h_0[:5, -1, :] * h_0[:5, -1, :]}")
+                # tqdm.write(f"h_0 final instance: {h_0[:5, -1, :]}")
             # loss = einops.reduce(error, "b i f -> i", "mean").sum()
             loss.backward()
             opt.step()
@@ -155,22 +178,21 @@ def optimize(
                 )
 
 
-def plot_intro_diagram(model: Model, filepath: Path) -> None:
+def plot_intro_diagram(model: Model, weight: torch.Tensor, filepath: Path) -> None:
     cfg = model.config
-    WA = model.A.detach() @ model.B.detach()
-    # WA = model.W.detach()
-    N = len(WA[:, 0])
+    N = len(weight[:, 0])
     sel = range(config.n_instances)  # can be used to highlight specific sparsity levels
-    plt.rcParams["axes.prop_cycle"] = plt.cycler(
-        "color",
-        plt.cm.viridis(model.importance[0].cpu().numpy()),  # type: ignore
-    )
+    # plt.rcParams["axes.prop_cycle"] = plt.cycler(
+    #     "color",
+    #     plt.cm.viridis(model.importance[0].cpu().numpy()),  # type: ignore
+    # )
     plt.rcParams["figure.dpi"] = 200
     fig, axs = plt.subplots(1, len(sel), figsize=(2 * len(sel), 2))
     for i, ax in zip(sel, axs, strict=False):
-        W = WA[i].cpu().detach().numpy()
+        W = weight[i].cpu().detach().numpy()
         colors = [mcolors.to_rgba(c) for c in plt.rcParams["axes.prop_cycle"].by_key()["color"]]
-        ax.scatter(W[:, 0], W[:, 1], c=colors[0 : len(W[:, 0])])
+        # ax.scatter(W[:, 0], W[:, 1], c=colors[0 : len(W[:, 0])])
+        ax.scatter(W[:, 0], W[:, 1])
         ax.set_aspect("equal")
         ax.add_collection(mc.LineCollection(np.stack((np.zeros_like(W), W), axis=1), colors=colors))  # type: ignore
 
@@ -183,7 +205,8 @@ def plot_intro_diagram(model: Model, filepath: Path) -> None:
             ax.spines[spine].set_visible(False)
         for spine in ["bottom", "left"]:
             ax.spines[spine].set_position("center")
-    plt.savefig(filepath)
+    # plt.savefig(filepath)
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -197,7 +220,14 @@ if __name__ == "__main__":
         n_features=5,
         n_hidden=2,
         n_instances=10,
-        k=10,
+        k=5,
+        n_batch=1024,
+        steps=20_000,
+        print_freq=100,
+        lr=1e-3,
+        lr_scale=cosine_decay_lr,
+        pnorm=0.5,
+        sparsity_coeff=0.01,
     )
 
     model = Model(
@@ -210,15 +240,32 @@ if __name__ == "__main__":
     )
     optimize(
         model,
-        n_batch=1024,
-        steps=20_000,
-        print_freq=100,
-        lr=1e-3,
-        lr_scale=cosine_decay_lr,
-        pnorm=0.5,
-        sparsity_coeff=0.0,
+        n_batch=config.n_batch,
+        steps=config.steps,
+        print_freq=config.print_freq,
+        lr=config.lr,
+        lr_scale=config.lr_scale,
+        pnorm=config.pnorm,
+        sparsity_coeff=config.sparsity_coeff,
     )
     # %%
-    plot_intro_diagram(model, filepath=Path(__file__).parent / "out" / "tms_factors_features.png")
+    print("Plot of W")
+    plot_intro_diagram(
+        model,
+        weight=model.A.detach() @ model.B.detach(),
+        # filepath=Path(__file__).parent / "out" / "tms_factors_features_W.png",
+        filepath=Path(__file__).parent
+        / "out"
+        / f"tms_factors_features_W_pnorm-{config.pnorm}_sparsity-{config.sparsity_coeff}_samples-{config.n_instances}.png",
+    )
+    print("Plot of B")
+    plot_intro_diagram(
+        model,
+        weight=model.B.detach(),
+        filepath=Path(__file__).parent
+        / "out"
+        / f"tms_factors_features_B_pnorm-{config.pnorm}_sparsity-{config.sparsity_coeff}_samples-{config.n_instances}.png",
+    )
+
 
 # %%
