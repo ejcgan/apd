4925fa16b982e5bfb854f66ccad9a882a5516e6b
Dan Braun <dan@apolloresearch.ai>
2024-09-09 12:30:29 +0000

    Simplify trig functions

--- spd/experiments/piecewise/trig_functions.py
+++ spd/experiments/piecewise/trig_functions.py
@@ -23,10 +23,10 @@ def generate_trig_functions(
     params = []
     for _ in range(num_trig_functions):
         a = torch.rand(1).item() * 2 - 1  # Uniform(-1, 1)
-        b = torch.exp(torch.rand(1) * 4 - 1).item()  # exp(Uniform(-1, 3))
+        b = torch.rand(1).item() * 0.9 + 0.1  # Uniform(0.1, 1)
         c = torch.rand(1).item() * 2 * torch.pi - torch.pi  # Uniform(-π, π)
         d = torch.rand(1).item() * 2 - 1  # Uniform(-1, 1)
-        e = torch.exp(torch.rand(1) * 4 - 1).item()  # exp(Uniform(-1, 3))
+        e = torch.rand(1).item() * 0.9 + 0.1  # Uniform(0.1, 1)
         f = torch.rand(1).item() * 2 * torch.pi - torch.pi  # Uniform(-π, π)
         g = torch.rand(1).item() * 2 - 1  # Uniform(-1, 1)
         trig_functions.append(create_trig_function(a, b, c, d, e, f, g))
