e75fda0864b3da13ca61b072c9f5105b0fa40179
Dan Braun <dan@apolloresearch.ai>
2025-01-26 15:42:20 +0000

    Simplify einsum names when removing parallel grads

--- spd/utils.py
+++ spd/utils.py
@@ -434,7 +434,7 @@ def calc_neuron_indices(
 
 @torch.inference_mode()
 def remove_grad_parallel_to_subnetwork_vecs(
-    A: Float[Tensor, "... d_in _"], A_grad: Float[Tensor, "... d_in _"]
+    A: Float[Tensor, "... d_in m"], A_grad: Float[Tensor, "... d_in m"]
 ) -> None:
     """Modify the gradient by subtracting it's component parallel to the activation.
 
@@ -443,11 +443,9 @@ def remove_grad_parallel_to_subnetwork_vecs(
     This is to stop Adam from changing the norm of A. Note that this will not completely prevent
     Adam from changing the norm due to Adam's (m/(sqrt(v) + eps)) term not preserving the norm
     direction.
-
-    The final dimension is k in the case of rank one and m in the case of full rank.
     """
-    parallel_component = einops.einsum(A_grad, A, "... d_in _, ... d_in _ -> ... _")
-    A_grad -= einops.einsum(parallel_component, A, "... _, ... d_in _ -> ... d_in _")
+    parallel_component = einops.einsum(A_grad, A, "... d_in m, ... d_in m -> ... m")
+    A_grad -= einops.einsum(parallel_component, A, "... m, ... d_in m -> ... d_in m")
 
 
 class SPDOutputs(NamedTuple):
